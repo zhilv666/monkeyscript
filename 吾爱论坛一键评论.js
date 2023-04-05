7a5e8e540253fcddc7bec7fd474f6b7a61fe4e36
zhilv666 <98442716+zhilv666@users.noreply.github.com>
2023-04-05 17:52:12 +0800

    Update 吾爱论坛一键评论.js

--- 吾爱论坛一键评论.js
+++ 吾爱论坛一键评论.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         吾爱论坛一键评论
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/zhilv666/monkeyscript/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       zhilv
 // @match        https://www.52pojie.cn/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=52pojie.cn
 // @grant        none
